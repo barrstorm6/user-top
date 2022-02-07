084add928a668917bce2d1418459beb3439f3a78
barrstorm6 <barrstorm6@gmail.com>
2022-02-07 18:21:01 +0600

    init commit

---
+++ src/main/java/com/barrstorm6/usertop/UserTopApplication.java
@@ -0,0 +1,13 @@
+package com.barrstorm6.usertop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserTopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserTopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barrstorm6/usertop/UserTopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barrstorm6.usertop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserTopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
